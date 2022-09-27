a1412b04968fc47cd967b3095414b32d1059b446
Азаревич Эдуард Александрович <azarevich_ea@peleng.jsc.local>
2022-09-27 14:14:04 +0300

    add fix timer

--- js/script.js
+++ js/script.js
@@ -40,15 +40,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2022-10-30';
+    const deadline = '2022-08-30';
 
     function getTimeremaining(endtime) {
+        let days, hours, minutes, seconds;
         //console.log(Date.parse(endtime) - Date.parse(new Date()));
-        const t = Date.parse(endtime) - Date.parse(new Date()),
+        const t = Date.parse(endtime) - Date.parse(new Date());
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
+                hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+                minutes = Math.floor((t / 1000 / 60) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+        }
 
         return {
             'total': t,
