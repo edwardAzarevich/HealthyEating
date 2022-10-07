044d9a4c7313cf7b4c272e528c2061b076313dea
Азаревич Эдуард Александрович <azarevich_ea@peleng.jsc.local>
2022-10-07 10:27:45 +0300

    add class for create card menu

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // Timer
 
-    const deadline = '2022-08-30';
+    const deadline = '2022-10-30';
 
     function getTimeremaining(endtime) {
         let days, hours, minutes, seconds;
@@ -118,6 +118,12 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    function openModel() {
+        modal.classList.toggle('show');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modelTimerId);
+    }
+
     function closeModel() {
         modal.classList.toggle('show');
         document.body.style.overflow = '';
@@ -135,6 +141,156 @@ window.addEventListener('DOMContentLoaded', () => {
         if (e.code === 'Escape' && modal.classList.contains('show')) {
             closeModel();
         }
-    })
+    });
+
+    //const modelTimerId = setTimeout(openModel, 10000);
+
+    // skroll 
+    function showModelByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModel();
+            window.removeEventListener('scroll', showModelByScroll);
+        }
+
+    }
+
+    window.addEventListener('scroll', showModelByScroll);
+
+
+    // Create Menu 
+    const params = {
+        nameMenu: "Фитнес",
+        text: "hi man",
+        price: 229
+    };
+    // Use class for card
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH();
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer;
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+        <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+        `;
+            this.parent.append(element);
+        }
+    };
+
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
+
+
+    // My code
+
+    /*class FormMenuItem {
+        constructor(cardItem) {
+            this.pathPhoto = cardItem.path;
+            this.nameMenu = cardItem.name;
+            this.text = cardItem.text;
+            this.price = cardItem.price;
+        }
+
+        createDivElement() {
+            const divMenuItem = document.createElement('div');
+            const divMenuItemPrice = document.createElement('div');
+            divMenuItem.classList = "menu__item";
+            divMenuItem.innerHTML += `<img src="${this.pathPhoto}" alt="vegy">`;
+            divMenuItem.innerHTML += `<h3 class="menu__item-subtitle">Меню "${this.nameMenu}"</h3>`;
+            divMenuItem.innerHTML += `<div class="menu__item-descr">Меню ${this.nameMenu}" - "${this.text}"</div>`;
+            divMenuItem.innerHTML += `<div class="menu__item-divider"></div>`;
+
+            divMenuItemPrice.classList = "menu__item-price";
+            divMenuItemPrice.innerHTML += `<div class="menu__item-cost">Цена:</div>`;
+            divMenuItemPrice.innerHTML += `<div class="menu__item-total"><span>${this.price}</span> грн/день</div>`;
+
+            divMenuItem.append(divMenuItemPrice);
+            return divMenuItem;
+        }
+
+    }
+
+    const CardList = [
+        {
+            name: "Фитнес",
+            path: "img/tabs/vegy.jpg",
+            text: 'это новый подход к приготовлению блюд:больше свежиховощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальнойценой и высоким качеством',
+            price: 229
+        },
+        {
+            name: "Премиум",
+            path: "img/tabs/elite.jpg",
+            text: "мы используем не только красивый дизайн упаковки, нои качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без походав ресторан!",
+            price: 550
+        },
+        {
+            name: "Постное",
+            path: "img/tabs/post.jpg",
+            text: "это тщательный подбор ингредиентов: полное отсутствиепродуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильноеколичество белков за счет тофу и импортных вегетарианских стейков.",
+            price: 430
+        },
+
+
+
+    ];
+
+    const devMenuList = document.querySelector('.menu .menu__field .container');
+
+    CardList.forEach(element => {
+        devMenuList.append(new FormMenuItem(element).createDivElement());
+
+
+    });
+
+*/
 
 });
+
+
