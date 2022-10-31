0da3b2acd021ef9bbb38192452a7ce9e57b4a8dc
Азаревич Эдуард Александрович <azarevich_ea@peleng.jsc.local>
2022-10-31 09:19:43 +0300

    add git ignore and npm (package-lock)

--- js/script.js
+++ js/script.js
@@ -300,4 +300,8 @@ window.addEventListener('DOMContentLoaded', function () {
             closeModal();
         }, 4000);
     }
+
+    this.fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
 });
