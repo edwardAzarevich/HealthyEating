d3f36f6c85c74bea36284771e4f75a903d3bdfed
Азаревич Эдуард Александрович <azarevich_ea@peleng.jsc.local>
2022-11-08 10:07:45 +0300

    add flipping animation slader

--- js/script.js
+++ js/script.js
@@ -315,50 +315,116 @@ window.addEventListener('DOMContentLoaded', function () {
         prev = this.document.querySelector('.offer__slider-prev'),
         next = this.document.querySelector('.offer__slider-next'),
         total = this.document.querySelector('#total'),
-        current = this.document.querySelector('#current');
-    let slideIndex = 1;
+        current = this.document.querySelector('#current'),
+        slidesWrapper = this.document.querySelector('.offer__slider-wrapper'),
+        slidesField = this.document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width;
 
-    showSlides(slideIndex);
+    let slideIndex = 1;
+    let offset = 0;
 
     if (slides.length < 10) {
         total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
     } else {
         total.textContent = slides.length;
+        current.textContent = slideIndex;
     };
 
-    function showSlides(n) {
-        if (n > slides.length) {
+    slidesField.style.width = 100 * slides.length + '%';
+
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    next.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        };
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
             slideIndex = 1;
-        };
-
-        if (n < 1) {
-            slideIndex = slides.length;
-        };
-
-        slides.forEach(item => {
-            item.classList.add('hide');
-        });
-
-        slides[slideIndex - 1].classList.add('show');
-        slides[slideIndex - 1].classList.remove('hide');
+        } else {
+            slideIndex++;
+        }
 
         if (slides.length < 10) {
             current.textContent = `0${slideIndex}`;
         } else {
             current.textContent = slideIndex;
-        };
-    }
-
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    };
+        }
+    });
 
     prev.addEventListener('click', () => {
-        plusSlides(-1);
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        };
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+
+
+
     });
 
-    next.addEventListener('click', () => {
-        plusSlides(1);
-    });
+    //showSlides(slideIndex);
+
+
+
+    // function showSlides(n) {
+    //     if (n > slides.length) {
+    //         slideIndex = 1;
+    //     };
+
+    //     if (n < 1) {
+    //         slideIndex = slides.length;
+    //     };
+
+    //     slides.forEach(item => {
+    //         item.classList.add('hide');
+    //     });
+
+    //     slides[slideIndex - 1].classList.add('show');
+    //     slides[slideIndex - 1].classList.remove('hide');
+
+    //     if (slides.length < 10) {
+    //         current.textContent = `0${slideIndex}`;
+    //     } else {
+    //         current.textContent = slideIndex;
+    //     };
+    // }
+
+    // function plusSlides(n) {
+    //     showSlides(slideIndex += n);
+    // };
+
+    // prev.addEventListener('click', () => {
+    //     plusSlides(-1);
+    // });
+
+    // next.addEventListener('click', () => {
+    //     plusSlides(1);
+    // });
 
 });
